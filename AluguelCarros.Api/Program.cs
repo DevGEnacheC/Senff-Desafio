0516aa088563eaf665a98c419046927e2109d3c9
DevGEnacheC <dev.genachec@gmail.com>
2025-05-08 21:33:34 -0300

    FIX Movendo pastas colocadas no projeto(Infrastructure) para Domain

--- AluguelCarros.Api/Program.cs
+++ AluguelCarros.Api/Program.cs
@@ -2,19 +2,15 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Add MediatR
 builder.Services.AddMediatR(typeof(Program).Assembly);
 
-// Configure Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
